aba0ea14c692e1d735cb0c06cb66efcad91558d0
Chris Peters <bildepunkt@gmail.com>
2021-11-17 21:15:42 -0800

    nail down layout, add svgs, etc.

--- src/Console.ts
+++ src/Console.ts
@@ -6,14 +6,22 @@ export interface ConsoleParams {
 
 export default class Console {
   private canvas: Canvas;
+
   private leftConElement: HTMLDivElement;
   private rightConElement: HTMLDivElement;
+  private dpadElement: HTMLDivElement;
+  private actionBtnIElement: HTMLDivElement;
+  private actionBtnIIElement: HTMLDivElement;
+
   private calculatedScreenDimensions: number = 256;
 
   constructor (params: ConsoleParams) {
     this.canvas = params.canvas;
     this.leftConElement = document.querySelector('#leftCon')!;
     this.rightConElement = document.querySelector('#rightCon')!;
+    this.dpadElement = document.querySelector('#dpad')!;
+    this.actionBtnIElement = document.querySelector('#I')!;
+    this.actionBtnIIElement = document.querySelector('#II')!;
 
     this.calculatePositionAndScale();
 
@@ -36,6 +44,12 @@ export default class Console {
         viewportHeight / viewportWidth < 2 ? viewportHeight / 2 : viewportWidth
     );
 
+    this.canvas.setPixelScale(this.calculatedScreenDimensions);
+
+    if (this.calculatedScreenDimensions / this.canvas.dimensions > this.canvas.pixelScale) {
+      this.calculatedScreenDimensions = this.canvas.dimensions * this.canvas.pixelScale;
+    }
+
     this.canvas.element.width = this.calculatedScreenDimensions;
     this.canvas.element.height = this.calculatedScreenDimensions;
 
@@ -56,6 +70,15 @@ export default class Console {
     const conWidth = this.calculatedScreenDimensions / 2;
     const conHeight = this.calculatedScreenDimensions;
     
+    const dpadDimensions = conWidth / 1.6;
+    const dpadLeft = (leftConLeft + conWidth / 2) - dpadDimensions / 2;
+    const dpadTop = (leftConTop + conHeight / 2) - dpadDimensions / 2;
+
+    const actionBtnDimensions = conWidth / 3.6;
+    const actionBtnSpacing = conWidth / 9;
+    const actionBtnCenterX = rightConLeft + conWidth / 2;
+    const actionBtnCenterY = rightConTop + conHeight / 2;
+    
     this.canvas.element.style.left = `${screenLeft}px`;
     this.canvas.element.style.top = `${screenTop}px`;
 
@@ -68,5 +91,20 @@ export default class Console {
     this.rightConElement.style.top = `${rightConTop}px`;
     this.rightConElement.style.width = `${conWidth}px`;
     this.rightConElement.style.height = `${conHeight}px`;
+
+    this.dpadElement.style.left = `${dpadLeft}px`;
+    this.dpadElement.style.top = `${dpadTop}px`;
+    this.dpadElement.style.width = `${dpadDimensions}px`;
+    this.dpadElement.style.height = `${dpadDimensions}px`;
+
+    this.actionBtnIElement.style.left = `${actionBtnCenterX - actionBtnSpacing / 2 - actionBtnDimensions}px`;
+    this.actionBtnIElement.style.top = `${actionBtnCenterY}px`;
+    this.actionBtnIElement.style.width = `${actionBtnDimensions}px`;
+    this.actionBtnIElement.style.height = `${actionBtnDimensions}px`;
+    
+    this.actionBtnIIElement.style.left = `${actionBtnCenterX + actionBtnSpacing / 2}px`;
+    this.actionBtnIIElement.style.top = `${actionBtnCenterY - actionBtnDimensions}px`;
+    this.actionBtnIIElement.style.width = `${actionBtnDimensions}px`;
+    this.actionBtnIIElement.style.height = `${actionBtnDimensions}px`;
   }
 }
--- test/Console.spec.ts
+++ test/Console.spec.ts
@@ -1,10 +1,11 @@
+import Canvas from '~src/Canvas';
 import Console from '../src/Console';
 
 describe('Console', () => {
   let console: Console;
 
   beforeEach(() => {
-    console = new Console();
+    console = new Console({ canvas: new Canvas() });
     global.innerWidth = 1024;
     global.innerHeight = 768;
   });
